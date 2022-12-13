aaf72ec693438eb9157779005e478590069f60c9
Hillary Forget <hillaryforget@outlook.com>
2022-12-12 19:51:24 -0500

    SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,3 +1,5 @@
+
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,19 +9,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2];
-const totalLength = process.argv[3];
-const values = [`%${cohortName}%`, totalLength];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     });
-  }).catch(err => console.error('query error', err.stack));
+  })
+  .catch(err => console.error('query error', err.stack));
